a34f66c9db9cb18aef56ebb4130f188166199922
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-06-03 19:35:07 +0800

    add jni

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,36 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=avcodec
+LOCAL_SRC_FILES :=FFmpeg/lib/libavcodec-56.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=avfilter  
+LOCAL_SRC_FILES :=FFmpeg/lib/libavfilter-5.so  
+include $(PREBUILT_SHARED_LIBRARY)  
+   
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=avformat
+LOCAL_SRC_FILES :=FFmpeg/lib/libavformat-56.so  
+include $(PREBUILT_SHARED_LIBRARY)  
+   
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=  avutil
+LOCAL_SRC_FILES :=FFmpeg/lib/libavutil-54.so  
+include $(PREBUILT_SHARED_LIBRARY)  
+   
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=  swresample
+LOCAL_SRC_FILES :=FFmpeg/lib/libswresample-1.so  
+include $(PREBUILT_SHARED_LIBRARY)  
+   
+include $(CLEAR_VARS)  
+LOCAL_MODULE :=  swscale
+LOCAL_SRC_FILES :=FFmpeg/lib/libswscale-3.so  
+include $(PREBUILT_SHARED_LIBRARY) 
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := showYUV
 LOCAL_SRC_FILES := ShowYUV.cpp
-
 include $(BUILD_SHARED_LIBRARY)
